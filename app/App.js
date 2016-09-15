95e8a04810a591afadfad88095e3b2e18626fe98
yoshiro <PMAC202S@PMAC202S.local>
2016-09-16 03:50:21 +0900

    routesを削除

--- app/App.js
+++ app/App.js
@@ -2,27 +2,17 @@
 
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-import Routes from './routes';
 
 import Template from './components/template';
-import Home from './layouts/home';
-import About from './layouts/about';
-import Products from './layouts/Products';
 import Post from './layouts/post';
 
 class App extends Component {
   render() {
-    return ((
-      <Router history={browserHistory}>
-        <Route path="/" component={Template}>
-          <IndexRoute component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/products" component={Products} />
-          <Route path="/post" component={Post} />
-        </Route>
-      </Router>
-    ));
+    return (
+      <Template>
+        <Post />
+      </Template>
+    )
   }
 }
 
